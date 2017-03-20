dc3b9064759bd9f8e1a5f29087bca603afc5441f
GrigoriyDidorenko <grigoriy.didorenko@gmail.com>
2017-03-20 22:55:44 +0200

    initial dependencies

---
+++ src/main/java/ua/com/zno/online/ZnoOnlineApplication.java
@@ -0,0 +1,12 @@
+package ua.com.zno.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZnoOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZnoOnlineApplication.class, args);
+	}
+}
---
+++ src/test/java/ua/com/zno/online/ZnoOnlineApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.com.zno.online;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZnoOnlineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
