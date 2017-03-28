33e9eee955a3e613c681f86e0dbf53a0b7c50b9a
GrigoriyDidorenko <grigoriy.didorenko@gmail.com>
2017-03-28 23:18:23 +0300

    simple mapping test created

---
+++ src/test/java/ua/com/zno/online/DTOs/EntityToDTOTest.java
@@ -0,0 +1,54 @@
+package ua.com.zno.online.DTOs;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import ua.com.zno.online.ZnoOnlineApplication;
+import ua.com.zno.online.domain.AbstractEntity;
+import ua.com.zno.online.domain.Answer;
+import ua.com.zno.online.domain.Question;
+import ua.com.zno.online.util.Constants;
+
+import java.util.Collections;
+
+/**
+ * Created by quento on 28.03.17.
+ */
+
+@SpringBootTest(classes = ZnoOnlineApplication.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class EntityToDTOTest {
+
+    @Autowired
+    private EntityToDTO entityToDTO;
+
+    @Test
+    public void testConvertToDTO() {
+
+        ua.com.zno.online.domain.Test test = Mockito.mock(ua.com.zno.online.domain.Test.class);
+
+        Question question = new Question();
+        question.setId(10L);
+        question.setDeleted(false);
+        question.setTest(test);
+        question.setQuestionText("Test question");
+
+        Answer answer = Mockito.mock(Answer.class);
+
+        question.setAnswers(Collections.singleton(answer));
+        test.setQuestions(Collections.singleton(question));
+
+        QuestionDTO questionDTO = entityToDTO.convertToDTO(question, QuestionDTO.class);
+
+
+        Assert.assertEquals(question.getId() + Constants.ID_APPENDER, questionDTO.getId().longValue());
+        Assert.assertEquals(question.getQuestionText(), questionDTO.getQuestionText());
+        Assert.assertEquals(question.getAnswers().size(), questionDTO.getAnswers().size());
+
+    }
+}
