c99207128517e4a54563ac478b11b5116d090d95
GrigoriyDidorenko <grigoriy.didorenko@gmail.com>
2017-03-29 21:36:19 +0300

    added mail service sketch

--- src/main/java/ua/com/zno/online/services/mail/MailService.java
+++ src/main/java/ua/com/zno/online/services/mail/MailService.java
@@ -1,22 +1,59 @@
 package ua.com.zno.online.services.mail;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.Future;
 
 /**
  * Created by quento on 28.03.17.
  */
 
+/*
 @Service
+@PropertySource("classpath:custom.properties")
 public class MailService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MailService.class.getName());
 
     //consider this realization https://github.com/ozimov/spring-boot-email-tools
 
-    @Async
-    public Future<Boolean> sendEmail() {
-        return new AsyncResult<>(false);
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value(value = "system.email")
+    private String from;
+
+    //add from (your mail to properties)
+
+    //could be tested : http://dolszewski.com/spring/sending-html-mail-with-spring-boot-and-thymeleaf/
+
+    @Async(value = "mailSendingExecutorPool")
+    public void sendEmail(String to, String topic, String content) {
+
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom(from);
+            messageHelper.setTo(to);
+            messageHelper.setSubject(topic);
+            messageHelper.setText(content);
+        };
+
+        try {
+            mailSender.send(messagePreparator);
+        } catch (MailException e) {
+            LOG.error("Failed to send message to {} with topic {} and content. Exception {} ", to, topic, content, e);
+        }
     }
+
+
 }
+*/
