970610687d99686fc26a9672de1406dd71b1fc3e
GrigoriyDidorenko <grigoriy.didorenko@gmail.com>
2017-03-29 21:37:35 +0300

    tread executor for email sender

--- src/main/java/ua/com/zno/online/beans/Beans.java
+++ src/main/java/ua/com/zno/online/beans/Beans.java
@@ -1,17 +1,35 @@
 package ua.com.zno.online.beans;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by quento on 26.03.17.
  */
-@Component
+@Configuration
+@EncryptablePropertySource(name = "application-prod", value = "classpath:application-prod.yml")
 public class Beans {
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean(name = "mailSendingExecutorPool")
+    public TaskExecutor mailSendingExecutorPool() {
+        return singleTaskExecutor();
+    }
+
+    private static TaskExecutor singleTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(1);
+        executor.setQueueCapacity(Integer.MAX_VALUE);
+        return executor;
     }
 }
