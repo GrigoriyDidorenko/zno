f57387773fdc8e30b79196c54049d80015b3fc94
GrigoriyDidorenko <grigoriy.didorenko@gmail.com>
2017-04-02 20:42:11 +0300

    swagger and spring security added
    (cherry picked from commit b1e96c7)

--- src/main/java/ua/com/zno/online/config/ApplicationConfiguration.java
+++ src/main/java/ua/com/zno/online/config/ApplicationConfiguration.java
@@ -6,13 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by quento on 26.03.17.
  */
 @Configuration
 @EncryptablePropertySource(name = "application-prod", value = "classpath:application-prod.yml")
+@EnableSwagger2
 @EnableTransactionManagement
 public class ApplicationConfiguration {
 
@@ -32,5 +37,14 @@ public class ApplicationConfiguration {
         executor.setMaxPoolSize(1);
         executor.setQueueCapacity(Integer.MAX_VALUE);
         return executor;
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
