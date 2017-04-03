3e75548a9ef08d1bd24af9ed9c738971969d3c42
Alexandr Balitsky <alexbalitskyformore@gmail.com>
2017-04-03 09:56:55 +0300

    vkauthorization simple

--- src/main/java/ua/com/zno/online/config/WebSecurityConfig.java
+++ src/main/java/ua/com/zno/online/config/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package ua.com.zno.online.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,13 +14,42 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
+    /*@Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().fullyAuthenticated().and().
                 httpBasic().and().
                 csrf().disable();
+    }*/
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/vkLogin", "/test/**", "/subject/**").permitAll()
+                .antMatchers("/smthImportant/**").hasRole("ADMIN")
+                .antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')")//just for exmpl
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .httpBasic()//for dev only
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/index");
+                //.logoutSuccessHandler(logoutSuccessHandler)
+                //.invalidateHttpSession(true)
+                //.addLogoutHandler(logoutHandler)
+                //.deleteCookies(cookieNamesToClear);
     }
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
 
 
 }
--- src/main/java/ua/com/zno/online/controllers/AuthenticationController.java
+++ src/main/java/ua/com/zno/online/controllers/AuthenticationController.java
@@ -1,21 +1,73 @@
 package ua.com.zno.online.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import sun.net.www.http.HttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * Created by obalitskiy on 3/31/17.
  */
 @Controller
 public class AuthenticationController {
+    /*@GetMapping("vkLogin")
+    public String login(@RequestParam(value = "access_token") Long at,
+                        @RequestParam(value = "expires_in") Long expires_in, @RequestParam(value = "user_id") Long user_id,
+                        @RequestParam(value = "email") Long email){
 
-    @GetMapping("vklogin")
-    public String login(@RequestParam(value = "userId") Long userId, @RequestParam(value = "email") String email, @RequestParam(value = "hash") String hash){
+        System.out.println();
+        return "";
+    }*/
+
+
+    @GetMapping("vkLogin")
+    public String login(@RequestParam String code) throws Exception {
+        System.out.println(2);
+
+        sendGet("https://oauth.vk.com/access_token?client_id=5948360&redirect_uri=http://localhost:8082/vkLogin&client_secret=InVGCG2rDUR7pwCWPyPo&code=" + code);
         System.out.println();
         return "";
     }
+
+    // HTTP GET request
+    private void sendGet(String url) throws Exception {
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
 
 
 }
