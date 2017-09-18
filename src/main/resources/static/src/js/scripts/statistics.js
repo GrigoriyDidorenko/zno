c1ff900e275417433d6b214aa53ce1afde0449a1
Katya <katya@MacBook-Pro-3.local>
2017-09-18 17:19:38 +0300

    added statistics empty

--- src/main/resources/static/src/js/scripts/statistics.js
+++ src/main/resources/static/src/js/scripts/statistics.js
@@ -10,7 +10,27 @@ $( document ).ready(function() {
             console.log(json);
 
             if(json.length == 0){
-                $('.statistic_nodata').css('display', 'block')
+
+                $('.statistic_nodata').css('display', 'flex');
+
+                jQuery.ajax({
+                    type: "GET",
+                    url: "api/subject",
+                    dataType: "json",
+                    contentType: "application/json; charset=utf-8",
+                    success: function (json) {
+                        console.log(json);
+                        var imgUrl;
+                        $.each(json, function (key, val) {
+                            imgUrl = val.name.toLowerCase().replace(/ /g, '_');
+                            imgUrl = 'src/img/' + imgUrl + '.svg';
+                            if(val.name.toLowerCase() == 'історія україни'){
+                                imgUrl = 'src/img/' + val.name.toLowerCase().replace(/ /g, '_') + '.png';
+                            }
+                            $('.pageSubjects').append('<a href="subject.html?subject=' + val.id + '" class="subject"><div class="subject_img"><img src='+imgUrl+' alt="' + val.name.toLowerCase() + '"><span>'+val.name+'</span></div></a>');
+                        });
+                    }
+                });
             }
 
             var avrgDuration;
